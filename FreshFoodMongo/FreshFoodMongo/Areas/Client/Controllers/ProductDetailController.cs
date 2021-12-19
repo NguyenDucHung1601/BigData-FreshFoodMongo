89c40416f70dfcf48aed8948c678cbfec8135841
Nguyễn Đức Hưng <48803420+NguyenDucHung1601@users.noreply.github.com>
2021-12-20 03:17:28 +0700

    RecommenderSystem

--- FreshFoodMongo/FreshFoodMongo/Areas/Client/Controllers/ProductDetailController.cs
+++ FreshFoodMongo/FreshFoodMongo/Areas/Client/Controllers/ProductDetailController.cs
@@ -4,6 +4,8 @@ using FreshFoodMongo.Models.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,8 +15,10 @@ namespace FreshFoodMongo.Areas.Client.Controllers
     {
         BaseDAO baseDao = new BaseDAO();
         SanPhamDAO spDao = new SanPhamDAO();
+        static List<SanPham> recommendProduct = new List<SanPham>();
+
         // GET: Client/ProductDetail
-        public ActionResult Index(Guid id)
+        public async Task<ActionResult> Index(Guid id)
         {
             SanPham sanpham = spDao.GetByID(id);
             sanpham.SoLuotXem = sanpham.SoLuotXem + 1;
@@ -27,7 +31,38 @@ namespace FreshFoodMongo.Areas.Client.Controllers
             obj.IDSanPham = id;
             obj.SoLuong = 1;
 
+            // recommend product
+            var idsRecommend = new List<Guid>();
+            if (Session["IDUSER_SESSION"] != null)
+            {
+                using (var client = new HttpClient())
+                {
+                    var path = string.Format(Common.CommonVariables.g_apiGetRecommendProdct, (Guid)Session["IDUSER_SESSION"]);
+                    var response = await client.GetAsync(path);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var data = await response.Content.ReadAsStringAsync();
+                        data = data.Replace("[", "").Replace("]", "").Replace("\"", "");
+                        var ids = data.Split(',');
+
+                        foreach (var item in ids)
+                            idsRecommend.Add(new Guid(item));
+                    }
+                }
+                IList<SanPham> list = new List<SanPham>();
+                foreach (var item in idsRecommend)
+                    list.Add(spDao.GetByID(item));
+                recommendProduct = list.ToList();
+            }
+
             return View(obj);
+        }
+
+        public ActionResult RecommendProduct(Guid idtheloai, string ten)
+        {
+            IList<SanPham> list = spDao.ListSanPham().Where(x => x.IDTheLoai == idtheloai).ToList();
+
+            return PartialView(recommendProduct);
         }
 
         public ActionResult RelatedProduct(Guid idtheloai, string ten)
--- FreshFoodMongo/FreshFoodMongo/Common/CommonVariables.cs
+++ FreshFoodMongo/FreshFoodMongo/Common/CommonVariables.cs
@@ -9,5 +9,6 @@ namespace FreshFoodMongo.Common
     {
         public static readonly string g_connectionString = @"mongodb://127.0.0.1:27017";
         public static readonly string g_databaseName = "FreshFoodHTH";
+        public static string g_apiGetRecommendProdct = @"https://localhost:44391/ProductRecommend?idKhachHang={0}";
     }
 }
